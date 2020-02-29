b8e3e9b9211d2883dcefc9b7c41e3843a7a93fda
William Kennedy <alexkenned@gmail.com>
2020-02-29 01:41:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def find_the_cheese(array)
   i = 0 
   while i < cheese_types.length do 
     if array.include?(cheese_types[i])
-      return cheese_types[i]
+      return "#{cheese_types[i]}"
     else 
       return nil 
     end
