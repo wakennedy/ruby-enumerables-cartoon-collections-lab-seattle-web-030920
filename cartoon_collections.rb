fff71a6def3e416c14d46e85d7a698ef4e2af744
William Kennedy <alexkenned@gmail.com>
2020-02-29 00:48:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,11 @@
 def roll_call_dwarves(dwarves)
-  hash = {}
   dwarves.each_with_index do |item, index|
     puts "#{index + 1}. #{item}"
   end
 end
 
 def summon_captain_planet# code an argument here
-  # Your code here
+  
 end
 
 def long_planeteer_calls# code an argument here
