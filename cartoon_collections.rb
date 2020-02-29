3ceb7442543d148771d62b38815490f7082d19ec
William Kennedy <alexkenned@gmail.com>
2020-02-29 01:32:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? {|array| array > 4 }
+  array.any? {|array| array.length > 4 }
 end
 
 def find_the_cheese(array)
