b58459021729d265e4b985898ee01685d8d4b9cb
William Kennedy <alexkenned@gmail.com>
2020-02-29 01:24:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,6 @@ def long_planeteer_calls(array)
   array.reduce {|array| array.length < 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
